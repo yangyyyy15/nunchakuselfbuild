123c03d6e29a9942e985f955e61275fc17a8548d
April Hu <aprilhu@mit.edu>
2025-04-26 13:30:31 +0800

    Updating quantized t5 encoder

---
+++ tests/flux/test_flux_qencoder.py
@@ -0,0 +1,22 @@
+import pytest
+
+from nunchaku.utils import get_precision, is_turing
+from .utils import run_test
+
+
+@pytest.mark.skipif(is_turing(), reason="Skip tests due to using Turing GPUs")
+@pytest.mark.parametrize(
+    "height,width,use_qencoder,expected_lpips",
+    [
+        (1024, 1024, True, 0.141),
+        (1920, 1024, True, 0.155)
+    ],
+)
+def test_int4_schnell_qencoder(height: int, width: int, use_qencoder: bool, expected_lpips: float):
+    run_test(
+        precision="int4",
+        height=height,
+        width=width,
+        use_qencoder=use_qencoder,
+        expected_lpips=expected_lpips,
+    )
