2d2ad378e7f8eda5ff5dcb792b023d7f1c776600
muyangli <lmxyy1999@foxmail.com>
2025-05-21 00:38:29 -0400

    debug building

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ if __name__ == "__main__":
     if "dev" in version:
         version = version + {date.today().strftime("%Y%m%d")}  # data
     version = version + "+torch" + torch_major_minor_version
+    print(version)
 
     ROOT_DIR = os.path.dirname(__file__)
 
