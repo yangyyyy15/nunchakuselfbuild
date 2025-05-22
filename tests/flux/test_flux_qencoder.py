2267f0ab8ea8d1591640d86b3f666e8a5f77cb84
muyangli <lmxyy1999@foxmail.com>
2025-05-21 20:39:27 -0400

    support fp4

--- tests/flux/test_flux_qencoder.py
+++ tests/flux/test_flux_qencoder.py
@@ -1,18 +1,15 @@
 import pytest
 
-from nunchaku.utils import is_turing
+from nunchaku.utils import is_turing, get_precision
 
 from .utils import run_test
 
 
 @pytest.mark.skipif(is_turing(), reason="Skip tests due to using Turing GPUs")
-@pytest.mark.parametrize(
-    "height,width,use_qencoder,expected_lpips",
-    [(1024, 1024, True, 0.1356), (1920, 1024, True, 0.1445)],
-)
-def test_int4_schnell_qencoder(height: int, width: int, use_qencoder: bool, expected_lpips: float):
+@pytest.mark.parametrize("height,width,use_qencoder,expected_lpips", [(1024, 1024, True, 0.1356)])
+def test_flux_schnell_qencoder(height: int, width: int, use_qencoder: bool, expected_lpips: float):
     run_test(
-        precision="int4",
+        precision=get_precision(),
         height=height,
         width=width,
         use_qencoder=use_qencoder,
